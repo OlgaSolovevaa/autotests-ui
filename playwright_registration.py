ba9eafa4f1f5f0b8f5f064612ae6934656386914
OlgaSolovevaa <160867605+OlgaSolovevaa@users.noreply.github.com>
2025-10-24 20:22:21 +0700

    5.4 Введение в Playwright. Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -6,14 +6,17 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    email_input = page.locator('//input[@id=":r0:"]')
+    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
     email_input.fill("user.name@gmail.com")
-    username_input = page.locator('//input[@id=":r1:"]')
+    username_input = page.get_by_test_id("registration-form-username-input").locator("input")
     username_input.fill("username")
-    password_input = page.locator('//input[@id=":r2:"]')
+    password_input = page.get_by_test_id("registration-form-password-input").locator("input")
     password_input.fill("password")
+    page.wait_for_timeout(5000)
     registration_button = page.get_by_test_id("registration-page-registration-button")
     registration_button.click()
-    wrong_email_or_password_alert = page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Dashboard")
+    dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text("Dashboard")
+
+
