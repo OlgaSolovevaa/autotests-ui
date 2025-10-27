e8de4a60b8d7d071b2331005b361d8f327ae71bd
OlgaSolovevaa <160867605+OlgaSolovevaa@users.noreply.github.com>
2025-10-27 22:14:58 +0700

    6.5 Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,44 @@
 import pytest
-from playwright.sync_api import Playwright, Page
+from playwright.sync_api import Playwright, Page, expect
 
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
+    browser.close()
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
+    email_input.fill("user.name@gmail.com")
+
+    username_input = page.get_by_test_id("registration-form-username-input").locator("input")
+    username_input.fill("username")
+
+    password_input = page.get_by_test_id("registration-form-password-input").locator("input")
+    password_input.fill("password")
+
+    registration_button = page.get_by_test_id("registration-page-registration-button")
+    registration_button.click()
+
+    dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text("Dashboard")
+
+    page.context.storage_state(path="./tests/browser-state.json")
+
+
+@pytest.fixture(scope="function")
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="./tests/browser-state.json")
+    yield context.new_page()
+    context.close()
     browser.close()
